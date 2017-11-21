7d7b0350edc629fb9977398956dad9f7cbb4d125
Diarmuid Kelly <diarmuid100@gmail.com>
2017-11-21 18:06:22 +0000

    Update V0.07 - branch

--- app/src/main/java/com/homecare/VCA/viewHolder/HomeCare.java
+++ app/src/main/java/com/homecare/VCA/viewHolder/HomeCare.java
@@ -11,5 +11,6 @@ public class HomeCare extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_care);
+        
     }
 }
